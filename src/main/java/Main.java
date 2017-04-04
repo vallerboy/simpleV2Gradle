0d23658ba7c1489d9e67eea922df6448e9d69223
Oskar Pol <oskarbank@gmail.com>
2017-04-04 19:18:01 +0200

    Lombok init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,28 @@
+import lombok.Cleanup;
+
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-       System.out.println("asd");
+      
+        Player player1 = Player.builder().age1(1)
+                .age(5)
+                .name("Siema")
+                .name3("Witaj")
+                .name1("takietam")
+                .age2(53).build();
+
+        @Cleanup
+        InputStream in = null;
+
+        try {
+          in = new FileInputStream(new File("tutaj_sciezka_do_pliku"));
+          //in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/Player.java
@@ -0,0 +1,19 @@
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Created by OskarPraca on 2017-04-04.
+ */
+
+@Data
+@Builder
+public class Player {
+    private int age;
+    private String name;
+    private int age1;
+    private String name1;
+    private int age2;
+    private String name2;
+    private int age3;
+    private String name3;
+}
